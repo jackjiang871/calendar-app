d08ead63456fd007a4c44b37207f6631d3299a95
Jack Jiang <jackjiang871@gmail.com>
2018-02-15 21:37:29 -0800

    Test

---
+++ Cal-endar/Schedule.swift
@@ -0,0 +1,15 @@
+//
+//  Schedule.swift
+//  Cal-endar
+//
+//  Created by Jack Jiang on 2/14/18.
+//  Copyright © 2018 jackjiang. All rights reserved.
+//
+
+import Foundation
+
+class Schedule {
+    
+    private var startTime = 0
+    private var endTime = 0
+}
